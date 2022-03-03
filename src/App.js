99abbf1ba824779be723c52c83923d08506b9ec9
ouissam-chebcheb <70104108+ouissam-chebcheb@users.noreply.github.com>
2022-03-03 19:46:11 +0100

    feat configure project backend and frontend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
